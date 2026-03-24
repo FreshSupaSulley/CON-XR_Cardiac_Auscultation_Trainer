83444216c8a0f33605b6f8dc197d0ed75f8fe6d6
Erich Boschert <erich.boschert10@gmail.com>
2026-03-24 10:46:30 -0400

    fix for actions

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base: "/CON-XR_Cardiac_Auscultation_Trainer/", // for our GitHub actions to be happy
     build: {
         // To match our GitHub pages repo
-        outDir: "docs"
+        outDir: "docs",
     }
-}
+})
